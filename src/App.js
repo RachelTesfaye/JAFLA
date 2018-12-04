0ad8c4e4ab362bf165250230ee997951508263c3
HenokTes72 <henoktes72@gmail.com>
2018-12-05 00:39:07 +0300

    edit a text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>This is the first push</h1>
+        <h1>Henok Tesfaye</h1>
       </div>
     );
   }
